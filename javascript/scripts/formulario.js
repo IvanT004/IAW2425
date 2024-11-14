a4f5abefa64797a927b080e154bb8e46be7e570e
Ivan Tellez <ivantellez04@iesamachado.org>
2024-11-14 14:35:44 +0100

    formulario con jQuery terminado

--- javascript/scripts/formulario.js
+++
@@ -1,9 +0,0 @@
-<script src="../jquery/jquery-3.7.1.js"></script>
-$(document).ready(function () {
-    $("button").click(function(){
-        for(let i = 0; i < $("input").length; i++)
-            if($("input").val()=="")
-                $("#" + i).html("Debe rellenar este campo");
-       
-    });
-});
