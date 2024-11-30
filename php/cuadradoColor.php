a898f8c7db4bda6e0d978c46188b9124d8817d34
Ivan Tellez <ivantellez04@iesamachado.org>
2024-11-30 13:33:32 +0100

    a

---
+++ php/cuadradoColor.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuadrado color</title>
+</head>
+<body>
+    <?php
+        echo rand(100000,999999);
+    ?>
+</body>
+</html>
