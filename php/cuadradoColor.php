82e1a1de6280efa06e79746e02bf2a21239154c9
Ivan <ivantellez04@iesamachado.org>
2024-11-30 14:31:00 +0100

    Ejercicio PHP acabados

--- php/cuadradoColor.php
+++ php/cuadradoColor.php
@@ -1,13 +1,19 @@
+<?php
+        $generado = rand(100000,999999);
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuadrado color</title>
+    <style>
+    #cuadrado{width: 300px; height:300px; background-color:<?php echo "#" . $generado;?>}
+    </style>
 </head>
 <body>
-    <?php
-        echo rand(100000,999999);
-    ?>
+    <div id="cuadrado"></div>
+    
 </body>
 </html>
