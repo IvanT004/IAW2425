9c36775c69204097c7673c38a07b9268860738b6
IvanT004 <ivantellez04@iesamachado.org>
2024-12-19 14:27:09 +0100

    a

---
+++ php/sessions.php
@@ -0,0 +1,16 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
+
+</body>
+</html> 
