7781512ded9eee6dc80948b851a7e22e2ef3e461
Ivan Tellez <ivantellez04@iesamachado.org>
2024-12-19 14:02:59 +0100

    cookies

--- php/contadorVisitasCookies.php
+++ php/contadorVisitasCookies.php
@@ -2,8 +2,7 @@
 if ( isset( $_COOKIE[ 'visitas' ] ) ) {
 
 setcookie( 'visitas', $_COOKIE[ 'visitas' ] + 1, time() + 3600 * 24 );
-setcookie('lang',"es",time()+3600*24);
-echo 'Numero de visitas: '.$_COOKIE[ 'visitas' ];
+echo "Numero de visitas: ". $_COOKIE['visitas'] . " usando el idioma " . $_COOKIE['lang'];
 }
 else {
 
