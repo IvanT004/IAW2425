66047fefd2d568f70205691916b13381c22bd016
Ivan Tellez <ivantellez04@iesamachado.org>
2025-01-09 14:00:25 +0100

    a

---
+++ php/cookies2.php
@@ -0,0 +1,14 @@
+<?php
+setcookie("nombre","Pepito conejo");ç
+//Cookie con matriz de datos
+setcookie("datos[nombre]", "Pepito");
+setcookie("datos[apellidos]", "Conejo");
+// Uso de la cookie
+if (isset($_COOKIE["nombre"])) {
+    print "<p>Su nombre es $_COOKIE[nombre]</p>\n";
+} else {
+    print "<p>No sé su nombre.</p>\n";
+}
+//Uso en caso de que se haya guardado una matriz en forma de cookies
+
+?>
