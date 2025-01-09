1e583afa468b714567cb438348e0d719eda3159c
Ivan Tellez <ivantellez04@iesamachado.org>
2025-01-09 14:13:35 +0100

    a

--- php/cookieSegura.php
+++ php/cookieSegura.php
@@ -1,4 +1,13 @@
 <?php
-    setcookie("fecha",$fecha);
-    header("Set-Cookie: name=value; HttpOnly");
+if ( isset( $_COOKIE[ 'visitas' ] ) ) {
+
+setcookie( 'visitas', $_COOKIE[ 'visitas' ] + 1, time() + 3600 * 24 );
+echo "Numero de visitas: ". $_COOKIE['visitas'] . " usando el idioma " . $_COOKIE['lang'];
+}
+else {
+
+setcookie( 'visitas', 1, time() + 3600 * 24 );
+header("Set-Cookie: name=value; HttpOnly");
+echo 'Bienvenido por primera vez a nuesta web';
+}
 ?>
