ea3086b68d73cf8502abbe7be4390f258bbd90a9
Ivan Tellez <ivantellez04@iesamachado.org>
2025-01-20 14:26:13 +0100

    a

--- php/MYSQL/registro.php
+++ php/MYSQL/registro.php
@@ -1,3 +1,51 @@
+<?php
+if(isset($_POST['enviar'])){
+
+    //Obtenemos los datos del usuario 
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $apellidos = htmlspecialchars($_POST['apellidos']);
+    $telefono = htmlspecialchars($_POST['telefono']);
+    $dni = htmlspecialchars($_POST['dni']);
+    $email = htmlspecialchars($_POST['email']);
+    $contrasena = htmlspecialchars($_POST['contrasena']);
+    $contrasena2 = htmlspecialchars($_POST['contrasena2']);
+    $provincia = htmlspecialchars($_POST['provincia']);
+    $codPostal = htmlspecialchars($_POST['codPostal']);
+
+    if ($contrasena !== $contrasena2){
+        die("Las contrasenas deben coincidir.");
+    }else{
+        $hashX = crypt($contrasena,"kk");
+        echo "Entro en else";
+        //Datos de conexión con la base de datos
+        $servername = "sql308.thsite.top";
+        $username = "thsi_38097482";
+        $password = "";
+        $bd = "thsi_38097482_BDIvan";
+        $enlace = mysqli_connect($servername,$username,$password,$bd);
+        if(!$enlace){
+            die("Ha ocurrido un error en la comunicación con la base de datos." . mysqli_connect_error());
+        }
+        
+        //Construcción de la consulta
+        $query = "INSERT INTO registro (nombre,apellidos,telefono,dni,correo,contrasena,provincia,codpostal) VALUES ('$nombre','$apellidos','$telefono','$dni','$email','$hashX','$provincia','$codPostal')";
+
+        //Ejecución de la consulta
+        $resultado = mysqli_query($enlace,$query);
+        if($resultado){
+            echo("Los datos se registraron correctamente");
+        }else{
+            echo("Hubo un error al registrar los datos");
+        }
+        
+        //Enviar correo
+        $mensaje = "¡Gracias por registrarte " . $nombre. " !" . "Se ha registrado con los siguientes datos: <br>Nombre: " . $nombre . "<br>Apellidos: " . $apellidos . "<br>Telefono: " . $telefono . "<br>DNI: " . $dni . "<br>Provincia: " . $provincia;
+
+        //Cerramos conexión
+        mysqli_close($enlace);
+    }
+} 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,59 +66,11 @@
         <div><input type='text' placeholder='Apellidos*' name='apellidos' required></div>
         <div><input type='text' placeholder='Teléfono*' name='telefono' required></div>
         <div><input type='text' placeholder='DNI*' name='dni' required></div>
-        <div><input type='text' placeholder='Correo electrónico*' name='email' required></div>
+        <div><input type='email' placeholder='Correo electrónico*' name='email' required></div>
         <div><input type='password' placeholder='Contraseña*' name='contrasena' required></div>
         <div><input type='password' placeholder='Repite la contraseña*' name='contrasena2' required></div>
         <div><input type='text' placeholder='Provincia*' name='provincia' required></div>
         <div><input type='text' placeholder='Código Postal' name='codPostal' ></div>
-        <button type='submit' name='enviar'>Crear cuenta</button>
+        <button type='submit' name='enviar' id='boton'>Crear cuenta</button>
     </form>
-
-<?php
-if(isset($_POST['enviar'])){
-
-    //Obtenemos los datos del usuario 
-    $nombre = htmlspecialchars($_POST['nombre']);
-    $apellidos = htmlspecialchars($_POST['apellidos']);
-    $telefono = htmlspecialchars($_POST['telefono']);
-    $dni = htmlspecialchars($_POST['dni']);
-    $email = htmlspecialchars($_POST['email']);
-    $contrasena = htmlspecialchars($_POST['contrasena']);
-    $contrasena2 = htmlspecialchars($_POST['contrasena2']);
-    $provincia = htmlspecialchars($_POST['provincia']);
-    $codPostal = htmlspecialchars($_POST['codPostal']);
-   
-    if($contrasena !== $contrasena2){
-        die("Las contraseñas deben coincidir.");
-    }else{
-        $hash = crypt($contrasena);
-
-        //Datos de conexión con la base de datos
-        $servername = 'sql308.thsite.top';
-        $username = 'thsi_38097482';
-        $password = '';
-        $bd = 'thsi_38097482_BDIvan';
-        $enlace = mysqli_connect($servername,$username,$password,$bd);
-        if(!$enlace){
-            die("Ha ocurrido un error en la comunicación con la base de datos.");
-        }
-
-        //Construcción de la consulta
-        $query = "INSERT INTO registro (Nombre,Apellidos,Telefono,DNI,CorreoElectronico,Contrasena,Provincia,CodigoPostal) VALUES ('$nombre','$apellidos','$telefono','$dni','$email','$hash','$provincia','$codPostal')";
-
-        //Ejecución de la consulta
-        $resultado = mysqli_query($enlace,$query);
-        if($resultado){
-            echo("Los datos se registraron correctamente");
-        }else{
-            echo("Hubo un error al registrar los datos");
-        }
-        //Enviar correo
-        $mensaje = "¡Gracias por registrarte $nombre!. Se ha registrado con los siguientes datos: <br>Nombre:$nombre<br>Apellidos:$apellidos<br>Telefono:$telefono<br>DNI:$dni<br>Provincia:$provincia";
-
-        //Cerramos conexión
-        mysqli_close($enlace);
-    }
-} 
-?>
 </body></html>
